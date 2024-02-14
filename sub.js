eafdf7a23a509ff8b1febc3b3dbe60f9ddc38d60
Aswini <aswinimuruganm532@gmail.com>
2024-02-14 09:29:43 +0530

    Html prgs

---
+++ sub.js
@@ -0,0 +1,6 @@
+function submit(){
+    a=document.getElementById("ainput").value;
+    b=document.getElementById("binput").value;
+    c=parseInt(a)-parseInt(b);
+    document.getElementById("answer").innerHTML=c;
+}
